253c5e97d4dffff1e7a3b59d3591c5433930245b
viktorrn <90602664+viktorrn@users.noreply.github.com>
2024-05-04 16:39:00 +0200

    Added shaders for pixelation

--- scenes/Camera.gd
+++ scenes/Camera.gd
@@ -3,29 +3,36 @@ extends Camera2D
 # Constants
 @export var RANDOM_SHAKE_STRENGTH: float = 30.0
 @export var SHAKE_DECAY_RATE: float = 5.0
+@export var TARGET_OFFSET: Vector2 = Vector2(0, -200.0)
 
 @onready var rand = RandomNumberGenerator.new()
 
 var noise_i: float = 0.0
 var shake_strength: float = 0.0
+var target: Node2D = null
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rand.randomize()
 	apply_shake(10)
+	target = get_node('../CharacterBody2D') as Node2D
 	pass # Replace with function body.
 
 func apply_shake(strenght: float):
 	self.shake_strength = strenght*5
-	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
 
 	shake_strength = lerp(shake_strength, 0.0, SHAKE_DECAY_RATE * delta)
 
-	self.offset = get_noise_offset() 
+	var target_position = self.global_position 
+	if target != null:
+		target_position = target.global_position + TARGET_OFFSET*3
+
+	self.offset = target_position + get_noise_offset() 
 	pass
 
 func get_noise_offset():
@@ -33,3 +40,6 @@ func get_noise_offset():
 		rand.randf_range(-shake_strength, shake_strength),
 		rand.randf_range(-shake_strength, shake_strength)
 	)
+
+func set_target(new_target: Node2D):
+	self.target = new_target
